f352883ffc07d34ff368f2ab02c8836e1928f3a0
Luke Switzer <10099969+lukeswitz@users.noreply.github.com>
2023-12-30 17:07:37 -0700

    Standardize AuthType in logs
    - Wigle standards use square brackets, so let’s do that!

--- AtomGPS_wigler.ino
+++ AtomGPS_wigler.ino
@@ -153,24 +153,24 @@ char* getAuthType(uint8_t wifiAuth) {
   static char authType[50];
   switch (wifiAuth) {
     case WIFI_AUTH_OPEN:
-      return "OPEN";
+      return "[OPEN]";
     case WIFI_AUTH_WEP:
-      return "WEP";
+      return "[WEP]";
     case WIFI_AUTH_WPA_PSK:
-      return "WPA_PSK";
+      return "[WPA_PSK]";
     case WIFI_AUTH_WPA2_PSK:
-      return "WPA2_PSK";
+      return "[WPA2_PSK]";
     case WIFI_AUTH_WPA_WPA2_PSK:
-      return "WPA_WPA2_PSK";
+      return "[WPA_WPA2_PSK]";
     case WIFI_AUTH_WPA2_ENTERPRISE:
-      return "WPA2_ENTERPRISE";
+      return "[WPA2_ENTERPRISE]";
     case WIFI_AUTH_WPA3_PSK:
-      return "WPA3_PSK";
+      return "[WPA3_PSK]";
     case WIFI_AUTH_WPA2_WPA3_PSK:
-      return "WPA2_WPA3_PSK";
+      return "[WPA2_WPA3_PSK]";
     case WIFI_AUTH_WAPI_PSK:
-      return "WAPI_PSK";
+      return "[WAPI_PSK]";
     default:
-      return "UNKNOWN";
+      return "[UNDEFINED]";
   }
 }
